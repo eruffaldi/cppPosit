e8724f75d8337e1a8ea80add32febc5e114104c2
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2018-08-20 19:47:21 +0200

    working float2bits constexpr updared

--- include/float2posit.hpp
+++ include/float2posit.hpp
@@ -6,11 +6,15 @@
 #include "posit.h"
 #include "floatconst2bits.hpp"
 
-template <class T,int totalbits, int esbits, class FT, bool withnan, class Trait>
-CONSTEXPR14 T float2posit(typename Trait::holder_t value)
+//template <class T,int totalbits, int esbits, class FT, bool withnan, class Trait>
+template <class PP, class Trait>
+CONSTEXPR14 PP float2posit(typename Trait::holder_t value)
 {
-	using PP =  Posit<T,totalbits,esbits,FT,withnan>;
-    using PT=typename Posit<T,totalbits,esbits,FT,withnan>::PT;
+	//using PP =  Posit<T,totalbits,esbits,FT,withnan>;
+    using PT=typename PP::PT; // trait
+    constexpr auto totalbits = PP::vtotalbits;
+    constexpr auto esbits = PP::vesbits;
+    using FT=typename PP::fraction_t;
     using POSIT_UTYPE = typename PT::POSIT_UTYPE;
     using POSIT_STYPE = typename PT::POSIT_STYPE;
     using ET=typename PT::exponenttype;
@@ -19,7 +23,7 @@ CONSTEXPR14 T float2posit(typename Trait::holder_t value)
     // Phase 2: float to expanded (Unpacked) handling special cases
 
     ET rawexp = bitset_getT(value,Trait::fraction_bits,Trait::exponent_bits) ;
-    auto negativeSign = value & (((typename Trait::holder_t)1) << (Trait::data_bits-1));
+    bool negativeSign = value & (((typename Trait::holder_t)1) << (Trait::data_bits-1));
 	auto exponentF = rawexp - Trait::exponent_bias; // ((un.u >> Trait::fraction_bits) & Trait::exponent_mask)
     auto fractionF = cast_right_to_left<typename Trait::holder_t,Trait::fraction_bits,FT,UT::POSIT_FRAC_TYPE_SIZE_BITS>()(value);
 
@@ -27,11 +31,11 @@ CONSTEXPR14 T float2posit(typename Trait::holder_t value)
 	{
 		if(fractionF == 0)
 		{
-			return negativeSign ? PT::POSIT_NINF : PT::POSIT_PINF;
+			return PP(typename PP::DeepInit(),negativeSign ? PT::POSIT_NINF : PT::POSIT_PINF);
 		}
 		else
 		{
-			return PT::POSIT_NAN;
+			return PP(typename PP::DeepInit(),PT::POSIT_NAN);
 		}
 	}
 	else if (rawexp == 0)
@@ -39,7 +43,7 @@ CONSTEXPR14 T float2posit(typename Trait::holder_t value)
         if(fractionF == 0)
         {
             negativeSign = false;
-            return 0;
+            return PP::zero();
         }
         else
         {
@@ -65,29 +69,38 @@ CONSTEXPR14 T float2posit(typename Trait::holder_t value)
 
     POSIT_UTYPE regbits = reg < 0 ? (PT::POSIT_HOLDER_MSB >> -reg) : (PT::POSIT_MASK << (PT::POSIT_HOLDER_SIZE-(reg+1))); // reg+1 bits on the left
 	POSIT_UTYPE eexp = msb_exp<POSIT_UTYPE,PT::POSIT_HOLDER_SIZE,esbits,(esbits == 00)>()(exp);
-	POSIT_STYPE p = ((fraction >> (rs+es+1)) | (eexp >> (rs+1)) | (regbits>>1)) >> (sizeof(T)*8-totalbits);
+	POSIT_STYPE p = ((fraction >> (rs+es+1)) | (eexp >> (rs+1)) | (regbits>>1)) >> (sizeof(PP)*8-totalbits);
 
-    return negativeSign ? -p : p;
+    return PP(typename PP::DeepInit(),negativeSign ? -p : p);
 }
 
-template <class T,int totalbits, int esbits, class FT, bool withnan, class Trait>
-T float2positF(typename Trait::value_t fvalue)
+template <class PP, class Trait>
+//template <class T,int totalbits, int esbits, class FT, bool withnan, class Trait>
+PP float2positF(typename Trait::value_t fvalue)
 {
     union {
         typename Trait::holder_t i;
         typename Trait::value_t  f; 
     } uu;
     uu.f = fvalue;
-    return float2posit<T,totalbits,esbits,FT,withnan,Trait>(uu.i);
+    return float2posit<PP,Trait>(uu.i);
 }
 
 
 #ifdef TESTFLOAT2POSIT
 
 // import struct; print("%04X" % struct.unpack("I",struct.pack('f',3.5)))
+using PP=Posit<int16_t,16, 2, uint16_t, false>;
 enum Q : int16_t { 
-    v1 = float2posit<int16_t,16, 2, uint16_t, false, single_trait>(0x40600000),
-    v2 = float2posit<int16_t,16, 2, uint16_t, false, single_trait>(float2bits(3.5f)),
+    v1 = float2posit<PP,single_trait>(0x40600000).v,
+    v2 = float2posit<PP,single_trait>(float2bits(3.5f)).v,
 
 };
+
+int main(int argc, char const *argv[])
+{
+    std::cout << PP(typename PP::DeepInit(),Q::v1) << " " << (float)PP(typename PP::DeepInit(),Q::v1) << std::endl;
+    std::cout << PP(typename PP::DeepInit(),Q::v2) << std::endl;
+    return 0;
+}
 #endif
