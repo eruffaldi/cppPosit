7b7b5ecde436a65923d679d91ddb1b29b711af27
Emanuele Ruffaldi <emanuele.ruffaldi@mmimicro.com>
2019-01-24 10:58:17 +0100

    fixes for VS2015

--- makeone.sh
+++ makeone.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cat <(echo -n "// cppPosit ") <(git rev-parse HEAD) <(echo "") include/bithippop.hpp include/fixedtraits.hpp include/floattraits.hpp include/typehelpers.hpp include/unpacked.h include/posit.h | grep -vEh '#include "[^\"]+"' | grep -vh "#pragma once" - > posit_one.h
+cat posit_one.h | pbcopy
+echo "copied in Clipboard if macOS"
